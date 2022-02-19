56103cf754ec45a5a26296a30022455554ec7877
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2022-02-19 14:17:05 -0300

    fixed style

--- codigo/stack/main.cpp
+++ codigo/stack/main.cpp
@@ -1,3 +1,25 @@
+// Copyright (c) 2022 Giovani Valdrighi
+
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+
 #include<iostream>
 
 using std::cout;
@@ -20,7 +42,7 @@ struct Node {
     No caso, o constructor irá criar um node com um valor já definido.
     E o ponteiro para o próximo igual a nulo.
     */
-    Node(T x) {
+    explicit Node(T x) {
         value = x;
         next = nullptr;
     }
@@ -30,7 +52,7 @@ struct Node {
     Comando "explicit" impede que o compilador faça 
     conversões de tipo "implicitas" (infere que é necessário a conversão).
     */
-    explicit Node(){
+    Node(){
         next = nullptr;
     }
 };
